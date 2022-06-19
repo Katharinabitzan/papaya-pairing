3668788f34b9f16dc69ba84ad979ab0864f78502
Katharinabitzan <katharinabitzan@gmail.com>
2022-06-19 22:27:02 +0100

    remove prelim API layers

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,8 +1,8 @@
 // index.js
 
-const vehicleController = require('../controller/vehicle');
-const modelController = require('../controller/model');
-const brandController = require('../controller/brand');
+// const vehicleController = require('../controller/vehicle');
+// const modelController = require('../controller/model');
+// const brandController = require('../controller/brand');
 
 
 module.exports = app => {
@@ -11,11 +11,4 @@ module.exports = app => {
             data: "Welcome to the Papaya Pairing API"
         })
     })
-
-    app.get("/brands", brandController.getAllBrands);
-    app.post("/brand/create", brandController.create);
-    app.put("/brand/:brandId", brandController.update);
-    // app.get("/v1/posts", modelController.getAllPostsOfUser);
-    // app.post("/api/post/create", modelController.createPost);
-    // app.put("/api/:postId", modelController.update);
 }
